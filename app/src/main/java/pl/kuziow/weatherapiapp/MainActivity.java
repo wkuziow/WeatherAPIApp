3ab78924dded566fee4f496dc01ad81c7a32eeb6
wkuziow <wkuziow@gmail.com>
2020-12-20 22:07:26 +0100

    buttons

--- app/src/main/java/pl/kuziow/weatherapiapp/MainActivity.java
+++ app/src/main/java/pl/kuziow/weatherapiapp/MainActivity.java
@@ -3,12 +3,91 @@ package pl.kuziow.weatherapiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_CityID, btn_getWeatherByID, btn_getWeatherByName;
+    EditText et_dataInput;
+    ListView lv_weatherReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_CityID = findViewById(R.id.btn_getCityID);
+        btn_getWeatherByID = findViewById(R.id.btn_getWeatherByCityID);
+        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+
+        et_dataInput = findViewById(R.id.et_dataInput);
+
+        lv_weatherReport = findViewById(R.id.lv_WeatherReports);
+
+
+        btn_CityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url = "https://www.metaweather.com/api/location/search/?query=london";
+
+// Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        BreakIterator textView;
+                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+// Add the request to the RequestQueue.
+                queue.add(stringRequest);
+
+
+                //Toast.makeText(MainActivity.this, "you clicked me 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Toast.makeText(MainActivity.this, "You clicked me 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, et_dataInput.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
